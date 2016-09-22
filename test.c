ccd43d86c6eab9c4b1b3f79034a823a274f95e2d
tbone <tbone@localhost.localdomain>
2016-09-22 23:38:49 +0530

    First code merger. Testing the tool.

---
+++ test.c
@@ -0,0 +1,13 @@
+/**
+ * 	This is a test program to test my git repo.
+ * 	Purpose to learn tool and code management. 
+ **/
+
+#include <stdio.h>
+
+int main(){
+	
+	printf("Hello World!!\n");
+
+	return 0;
+}	
